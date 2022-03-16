c708f24d9870b319eeae79fb11b819abd3b79d0c
tharindud <tharindua@wso2.com>
2022-03-16 17:28:29 +0530

    fix error handling

--- components/okta.key.manager/src/main/java/org/wso2/okta/client/model/OktaDCRClient.java
+++ components/okta.key.manager/src/main/java/org/wso2/okta/client/model/OktaDCRClient.java
@@ -3,27 +3,28 @@ package org.wso2.okta.client.model;
 import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
+import org.wso2.carbon.apimgt.impl.kmclient.KeyManagerClientException;
 
 public interface OktaDCRClient {
 
     @RequestLine("POST")
     @Headers("Content-Type: application/json")
-    public ClientInfo createApplication(ClientInfo clientInfo);
+    public ClientInfo createApplication(ClientInfo clientInfo) throws KeyManagerClientException;
 
 
     @RequestLine("GET /{clientId}")
     @Headers("Content-Type: application/json")
-    public ClientInfo getApplication(@Param("clientId") String clientId);
+    public ClientInfo getApplication(@Param("clientId") String clientId) throws KeyManagerClientException;
 
     @RequestLine("PUT /{clientId}")
     @Headers("Content-Type: application/json")
-    public ClientInfo updateApplication(@Param("clientId") String clientId,ClientInfo clientInfo);
+    public ClientInfo updateApplication(@Param("clientId") String clientId, ClientInfo clientInfo) throws KeyManagerClientException;
 
     @RequestLine("DELETE /{clientId}")
     @Headers("Content-Type: application/json")
-    public void deleteApplication(@Param("clientId") String clientId);
+    public void deleteApplication(@Param("clientId") String clientId) throws KeyManagerClientException;
 
     @RequestLine("PUT /{clientId}/lifecycle/newSecret")
     @Headers("Content-Type: application/json")
-    public ClientInfo regenerateClientSecret(@Param("clientId") String clientId);
+    public ClientInfo regenerateClientSecret(@Param("clientId") String clientId) throws KeyManagerClientException;
 }
